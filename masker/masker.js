e238d48832525ac24ea88507d33495f7a57a6625
Hassan Ejaz <hassan_ejaz@astutesol.com>
2018-08-07 15:11:10 +0500

    Added new plugin masker

---
+++ masker/masker.js
@@ -0,0 +1,18 @@
+var inps = document.querySelectorAll('[data-mask]');
+for(var i = 0; i < inps.length; i++){
+  inps[i].onkeypress = function(e){
+    var mask = this.getAttribute('data-mask');
+    if(this.value.length < mask.length){
+      var start = this.value.length;
+      var end = this.value.length + 1;
+      //alert(start + '-' + end);
+      var mask_char = mask.substring(start, end);
+      if(mask_char != 'x'){
+        this.value += mask_char;
+      }
+    }else{
+      e.preventDefault();
+      return false;
+    }
+  }
+}
