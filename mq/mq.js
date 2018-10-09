c687c9934c86a5cb181b86050db511739ddc3468
Hassan Ejaz <hassan_ejaz@astutesol.com>
2018-10-09 10:44:33 +0500

    updated window.onload

--- mq/mq.js
+++ mq/mq.js
@@ -1,47 +1,49 @@
-function _mq(selector) {
-    this.selector = selector;
-	this.on = function(operator, width, obj){
-		if(operator.trim() == '<'){
-			var style = document.createElement('style');
-			var media = '@media(max-width: ' + width.split("-")[0].trim() + ')';
-			media += "{";
-			media += this.selector + "{";
-			for (var i in obj) {
-				media += i + ": " + obj[i] + ";";
-			}
-			media += "}";
-			media += "}";
-			style.innerText = media;
-			document.head.appendChild(style);
-		}else if(operator.trim() == '>'){
-			var style = document.createElement('style');
-			var media = '@media(min-width: ' + width.split("-")[0].trim() + ')';
-			media += "{";
-			media += this.selector + "{";
-			for (var i in obj) {
-				media += i + ": " + obj[i] + ";";
-			}
-			media += "}";
-			media += "}";
-			style.innerText = media;
-			document.head.appendChild(style);
-		}else if(operator.trim() == '|'){
-			if(width.trim().indexOf('-') > -1){
-				var style = document.createElement('style');
-				var media = '@media(min-width: ' + width.split("-")[0].trim() + ') and (max-width: ' + width.split("-")[1].trim() + ')';
-				media += "{";
-				media += this.selector + "{";
-				for (var i in obj) {
-					media += i + ": " + obj[i] + ";";
-				}
-				media += "}";
-				media += "}";
-				style.innerText = media;
-				document.head.appendChild(style);
-			}
-		}else{
-			console.error("Invalid operator for _mq('"+operator+"', '"+width+"', object). Supported operators are '>', '<', '|'");
-		}
-	}
-    return this;
+window.onload = function(){
+    function _mq(selector) {
+        this.selector = selector;
+    	this.on = function(operator, width, obj){
+    		if(operator.trim() == '<'){
+    			var style = document.createElement('style');
+    			var media = '@media(max-width: ' + width.split("-")[0].trim() + ')';
+    			media += "{";
+    			media += this.selector + "{";
+    			for (var i in obj) {
+    				media += i + ": " + obj[i] + ";";
+    			}
+    			media += "}";
+    			media += "}";
+    			style.innerText = media;
+    			document.head.appendChild(style);
+    		}else if(operator.trim() == '>'){
+    			var style = document.createElement('style');
+    			var media = '@media(min-width: ' + width.split("-")[0].trim() + ')';
+    			media += "{";
+    			media += this.selector + "{";
+    			for (var i in obj) {
+    				media += i + ": " + obj[i] + ";";
+    			}
+    			media += "}";
+    			media += "}";
+    			style.innerText = media;
+    			document.head.appendChild(style);
+    		}else if(operator.trim() == '|'){
+    			if(width.trim().indexOf('-') > -1){
+    				var style = document.createElement('style');
+    				var media = '@media(min-width: ' + width.split("-")[0].trim() + ') and (max-width: ' + width.split("-")[1].trim() + ')';
+    				media += "{";
+    				media += this.selector + "{";
+    				for (var i in obj) {
+    					media += i + ": " + obj[i] + ";";
+    				}
+    				media += "}";
+    				media += "}";
+    				style.innerText = media;
+    				document.head.appendChild(style);
+    			}
+    		}else{
+    			console.error("Invalid operator for _mq('"+operator+"', '"+width+"', object). Supported operators are '>', '<', '|'");
+    		}
+    	}
+        return this;
+    }
 }
